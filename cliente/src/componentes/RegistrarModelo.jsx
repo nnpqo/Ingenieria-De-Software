1a67f0480ef25e0fe35dd05eae3eb0428cdbcf18
jesus~ <jesusbrere@gmail.com>
2023-03-26 15:55:28 -0400

    pagina principal

---
+++ cliente/src/componentes/RegistrarModelo.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+const RegistrarModelo =()=>{
+    return(
+        <div>
+            <p>REGISTRAR MODELO DE DISPOSITIVO</p>
+            <form>
+                <div id="huImagen">
+
+                </div>
+                <p>Nombre</p>
+                <input type="Text" required minLength="2" maxLength="30" pattern="^[A-Z]+$" />
+                <p>Descripcion</p>
+                <input type="Text"required minLength="10" maxLength="200"/>
+                <p>Etiquetas</p>
+                <select id="SelectEtiquetas">
+                    <option value='Apple'>Apple</option>
+                    <option value='Xiaomi'>Xiaomi</option>
+                    <option value='Samsumg'>Samsung</option>
+                </select>
+                <input type="submit"></input>
+                <input type="reset"></input>
+            </form>
+
+        </div>
+
+
+    );  
+};
+export default RegistrarModelo;

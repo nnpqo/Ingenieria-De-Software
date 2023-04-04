501ec60f8c44eedc1ab5205c010cc8853c80e919
Ivan Palacios <ivpalacios47@gmail.com>
2023-04-04 17:04:45 -0400

    productos alineados

--- cliente/src/componentes/Ventana.js
+++ cliente/src/componentes/Ventana.js
@@ -1,66 +1,94 @@
 import { Boton } from "./Boton";
-import { setModeloDispositivo,updateModeloDispositivo } from "../API/api";
+import { setModeloDispositivo, updateModeloDispositivo } from "../API/api";
 import { ImagenFormulario } from "./ImagenFormulario";
 import { CajaTexto } from "./CajaTexto";
 import { ComboBox } from "./ComboBox";
 import "../estilos/ventana.css";
 import "../estilos/boton.css";
 import { Aviso } from "./Aviso";
-import {Producto} from "./Producto";
+import { Producto } from "./Producto";
 
 export const VentanaFormulario = (props) => {
   //const modelos= Object.values(data);
   const modelos = ["p30 pro", "galaxy s20", "redmi note 11"];
   //const etiquetas= Object.values(data);
   const etiquetas = ["xiaomi", "samsumg"];
-  const opcionesModificar = <>
-    <ComboBox nombre={"Modelo*"} opciones={modelos} id={'modelo'} />
-    <CajaTexto nombre={"cambiar Nombre*"}  id={'nombreModelo'}/>
-    <CajaTexto nombre={"cambiar Descripcion*"}  id={'descripcion'}/>
-    <ComboBox nombre={"Etiquetas"} opciones={etiquetas} id={'etiqueta'}/>
+  const opcionesModificar = (
+    <>
+      <ComboBox nombre={"Modelo*"} opciones={modelos} id={"modelo"} />
+      <CajaTexto nombre={"cambiar Nombre*"} id={"nombreModelo"} />
+      <CajaTexto nombre={"cambiar Descripcion*"} id={"descripcion"} />
+      <ComboBox nombre={"Etiquetas"} opciones={etiquetas} id={"etiqueta"} />
     </>
-  const opcionesRegistro = <>
-    <CajaTexto nombre={"Nombre*"} id={'nombreModelo'}/>
-    <CajaTexto nombre={"Descripción*"} id={'descripcion'}/>
-    <ComboBox nombre={"Etiquetas*"} opciones={etiquetas} id={'etiqueta'}/>
+  );
+  const opcionesRegistro = (
+    <>
+      <CajaTexto nombre={"Nombre*"} id={"nombreModelo"} />
+      <CajaTexto nombre={"Descripción*"} id={"descripcion"} />
+      <ComboBox nombre={"Etiquetas*"} opciones={etiquetas} id={"etiqueta"} />
     </>
-
+  );
 
   return (
     <div className="ventana-formulario">
-      <p className="direccion"> Producto &gt; {props.tipo === 'registro' ?
-       'Registrar modelo de dispositivo movil' : 'Modificar dispositivo'} </p>
+      <p className="direccion">
+        {" "}
+        Producto &gt;{" "}
+        {props.tipo === "registro"
+          ? "Registrar modelo de dispositivo movil"
+          : "Modificar dispositivo"}{" "}
+      </p>
       {props.imagen === true && <ImagenFormulario />}
       <div className="titulo-Ventana">
         <h1>{props.titulo}</h1>
       </div>
       <div className="formulario">
-        {props.tipo === 'registro' && opcionesRegistro}
-        {props.tipo === 'modificar' && opcionesModificar}
-        </div>
+        {props.tipo === "registro" && opcionesRegistro}
+        {props.tipo === "modificar" && opcionesModificar}
+      </div>
       <div className="Guardar-Cancelar">
         <Boton
           nombre={"GUARDAR"}
           estilos={"guardar"}
-          funcion={()=>{if (props.tipo== 'registro') setModeloDispositivo();
-                        else updateModeloDispositivo()}}
+          funcion={() => {
+            if (props.tipo == "registro") setModeloDispositivo();
+            else updateModeloDispositivo();
+          }}
         />
-        <Aviso nombre="CANCELAR" mensaje="esto es el mensaje" estilos={"cancelar"}></Aviso>
+        <Aviso
+          nombre="CANCELAR"
+          mensaje="esto es el mensaje"
+          estilos={"cancelar"}
+        ></Aviso>
       </div>
     </div>
   );
 };
 
-
 export const Ventana = (props) => {
-  
   return (
-    <div className="ventana-modelos">
+    <div>
       <p>categorias &gt; dispositivos moviles &gt; samsumg</p>
-      <Producto />
-      <Producto />
-      <Producto />
-      
+      <div className="ventana-productos">
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+        <Producto />
+      </div>
     </div>
   );
 };

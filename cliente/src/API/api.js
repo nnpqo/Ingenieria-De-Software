29153545e1854dcef542535300768ca5f21d42cb
Ivan Palacios <ivpalacios47@gmail.com>
2023-04-04 23:48:24 -0400

    validacion

--- cliente/src/API/api.js
+++ cliente/src/API/api.js
@@ -1,26 +1,51 @@
 import axios from "axios";
-
 const instancia = axios.create({
   baseURL: "http://localhost:3001",
 });
 
 export const imagen = (event) => {
+  const pesoMax = 1048576;
+  const minAncho = 250;
+  const minAlto = 250;
   const file = event.target.files[0];
-  const formData = new FormData();
-  formData.append("image", file);
-  instancia
-    .post("/subirImagenes", formData)
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+  if (!file.type.startsWith("image/")) {
+    console.log("hello")
+    return "el archivo no es una imagen";
+    
+  } else {
+    if (file.size > pesoMax) {
+      console.log("hhhhhh")
+      return "el tamaño de la imagen es superior a la permitida";
+    } else {
+      const img = new Image();
+      img.onload = () => {
+        console.log(img.width);
+        console.log(img.height);
+        if (img.width < minAlto || img.height < minAncho) {
+          return "la resolucion de la imagen es menor a la permitida";
+  
+        }
+        const formData = new FormData();
+        formData.append("image", file);
+        instancia
+          .post("/subirImagenes", formData)
+          .then((res) => console.log(res))
+          .catch((err) => console.log(err));
+      };
+        
+    }
+  }
 };
 
 export const setModeloDispositivo = () => {
-  let nombre=document.getElementById("nombreModelo").value;
-  let descripcion=document.getElementById("descripcion").value;
-  let etiqueta=document.getElementById("etiqueta").value;
-  const datos={nombre: nombre,
-              descripcion: descripcion,
-              etiqueta: etiqueta}
+  let nombre = document.getElementById("nombreModelo").value;
+  let descripcion = document.getElementById("descripcion").value;
+  let etiqueta = document.getElementById("etiqueta").value;
+  const datos = {
+    nombre: nombre,
+    descripcion: descripcion,
+    etiqueta: etiqueta,
+  };
   console.log(datos);
   /*instancia
     .post(url + "", datos)
@@ -29,14 +54,16 @@ export const setModeloDispositivo = () => {
 };
 
 export const updateModeloDispositivo = () => {
-  let modelo=document.getElementById("modelo").value;
-  let nombre=document.getElementById("nombreModelo").value;
-  let descripcion=document.getElementById("descripcion").value;
-  let etiqueta=document.getElementById("etiqueta").value;
-  const datos={modelo: modelo,
-              nombre: nombre,
-              descripcion: descripcion,
-              etiqueta: etiqueta}
+  let modelo = document.getElementById("modelo").value;
+  let nombre = document.getElementById("nombreModelo").value;
+  let descripcion = document.getElementById("descripcion").value;
+  let etiqueta = document.getElementById("etiqueta").value;
+  const datos = {
+    modelo: modelo,
+    nombre: nombre,
+    descripcion: descripcion,
+    etiqueta: etiqueta,
+  };
   console.log(datos);
   /*instancia
     .post(url + "", datos)
@@ -44,7 +71,6 @@ export const updateModeloDispositivo = () => {
     .catch();*/
 };
 export const getModeloDispositivos = () => {
-
   instancia
     .get("/")
     .then(function (response) {
--- servidor/imagen.js
+++ servidor/imagen.js
@@ -12,11 +12,6 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage,
   dest: path.join(__dirname, "public/imagenes"),
-  fileFilter: (req, file, cb) => {
-    if (!file.mimetype.startsWith("image/")) {
-      return cb("El archivo no es una imagen");
-    }
-    cb(null, true)},
 }).single("image");
 
 

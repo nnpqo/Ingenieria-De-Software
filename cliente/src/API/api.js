18e5adcaeb88f7a8187df2687039979b3686150f
Ivan Palacios <ivpalacios47@gmail.com>
2023-04-06 00:44:32 -0400

    hola

--- cliente/src/API/api.js
+++ cliente/src/API/api.js
@@ -3,36 +3,44 @@ const instancia = axios.create({
   baseURL: "http://localhost:3001",
 });
 
+export let validacion = "";
+let valido = false;
+
+// export const imagen = (event) => {
+//   const file = previzualizar;
+// };
+
 export const imagen = (event) => {
   const pesoMax = 1048576;
   const minAncho = 250;
   const minAlto = 250;
   const file = event.target.files[0];
+  valido = false;
   if (!file.type.startsWith("image/")) {
-    console.log("hello")
-    return "el archivo no es una imagen";
-    
+    console.log("no es imagen");
+    validacion = "el archivo no es una imagen";
   } else {
     if (file.size > pesoMax) {
-      console.log("hhhhhh")
-      return "el tamaño de la imagen es superior a la permitida";
+      console.log("tamaño de la imagen muy alta");
+      validacion = "el tamaño de la imagen es superior a la permitida";
     } else {
       const img = new Image();
+      img.src = URL.createObjectURL(file);
       img.onload = () => {
         console.log(img.width);
         console.log(img.height);
         if (img.width < minAlto || img.height < minAncho) {
-          return "la resolucion de la imagen es menor a la permitida";
-  
+          console.log("resolusion demaciado baja");
+          validacion = "la resolucion de la imagen es menor a la permitida";
+        } else {
+          const formData = new FormData();
+          formData.append("image", file);
+          instancia
+            .post("/subirImagenes", formData)
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
         }
-        const formData = new FormData();
-        formData.append("image", file);
-        instancia
-          .post("/subirImagenes", formData)
-          .then((res) => console.log(res))
-          .catch((err) => console.log(err));
       };
-        
     }
   }
 };
--- cliente/src/componentes/ImagenFormulario.js
+++ cliente/src/componentes/ImagenFormulario.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Boton } from "./Boton";
-import { imagen } from "../API/api";
+import { imagen, validacion } from "../API/api";
 import imagenCarga from "../img.png";
 
 export const ImagenFormulario = () => {
+  const [val, setValidacion] = useState(validacion);
+
   return (
     <div className="imagen-formulario">
       <label for="image" class="custom-file-upload">
-        <img src={imagenCarga} alt="Icono de carga de archivos" />
+        <img src={imagenCarga} alt="Icono de carga de archivos"  id="previzualizar"/>
         <span>Subir imagen</span>
       </label>
-      <input type="file" id="image" onChange={imagen} style={{display: "none"}} />
+      <input
+        type="file"
+        id="image"
+        onChange={imagen}
+        style={{ display: "none" }}
+      />
+      <span>{val}</span>
     </div>
   );
 };

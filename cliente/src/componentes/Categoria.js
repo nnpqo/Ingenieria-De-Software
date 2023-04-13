4bde69663509309db2a60803d11c9705e040fbd7
Ivan Palacios <ivpalacios47@gmail.com>
2023-04-12 23:50:08 -0400

    add categoria

---
+++ cliente/src/componentes/Categoria.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { getEtiquetas } from "../API/api";
+import "../estilos/app.css"
+
+export const Categoria = (props) => {
+  const [desplegado, setdesplegado] = useState(false);
+  const [etiquetas, setEtiquetas] = useState([]);
+ 
+
+  useEffect(()=>{
+    const etiquetas = async () => {
+      await getEtiquetas().then((nombres) => setEtiquetas(nombres));}
+    etiquetas();
+  },[])
+
+
+  let et = etiquetas?.map((eti)=>{
+    return <ul>
+      <Etiqueta nombre={eti}
+      isChecked={false}
+      handleCheckboxSelection={props.manejarSelecciónSelection}/>
+    </ul>
+  })
+ 
+  return (
+    <div className="categoria">
+      <span className="dispositivosMoviles">
+        <a
+          onClick={() => {
+            setdesplegado(!desplegado);
+          }}
+        >
+          Dispositivos móviles
+        </a>
+      </span>
+      <li className={desplegado ? "etiquetas-visible" : "etiquetas"}>
+        {et}
+      </li>
+    </div>
+  );
+};
+const Etiqueta = (props) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  function handleChange() {
+    setIsChecked(!isChecked);
+    props.handleCheckboxSelection(props.nombre, !isChecked);
+  }
+
+  useEffect(() => {
+    setIsChecked(props.isChecked);
+  }, [props.isChecked]);
+
+  return (
+    <>
+      <input type="checkbox" 
+      name={props.nombre} 
+      id="check"
+      checked={isChecked}
+      onChange={handleChange}/>
+      <span className="nombre-etiqueta">{props.nombre}</span>
+    </>
+  );
+};

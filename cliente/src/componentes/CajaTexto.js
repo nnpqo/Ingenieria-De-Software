f5f4bb3c71767a2e2527b3a4338dc848c2c9c5e5
jhamil06 <201801948@est.umss.edu>
2023-04-23 22:58:09 -0400

    mensajeerrorcaracteres

--- cliente/src/componentes/CajaTexto.js
+++ cliente/src/componentes/CajaTexto.js
@@ -15,9 +15,9 @@ export const CajaTexto = (props) => {
       setErrorMsg(`El valor ingresado es demasiado corto (mínimo ${props.min} caracteres)`); // Establece un mensaje de error personalizado si el valor es demasiado corto
     } else if (event.target.value.length > props.max) {
       setErrorMsg(`El valor ingresado es demasiado largo (máximo ${props.max} caracteres)`); // Establece un mensaje de error personalizado si el valor es demasiado largo
-    } /*else if (event.target.value.match(props.regex)) {
+    } else if (event.target.value.match(props.regex)) {
       setErrorMsg(`El valor ingresado contiene caracteres no permitidos`); // Establece un mensaje de error personalizado si el valor contiene caracteres no permitidos
-    } */else {
+    } else {
       setErrorMsg(''); // Si no hay errores de validación, establece 'errorMsg' en una cadena vacía
     }
   };

fb09855e21e85aa72a0a1b3296cf7f955c20caa3
jhamil06 <201801948@est.umss.edu>
2023-04-25 20:49:06 -0400

    mensajedeerrorendescripcion

--- cliente/src/componentes/CajaTexto.js
+++ cliente/src/componentes/CajaTexto.js
@@ -13,8 +13,6 @@ export const CajaTexto = (props) => {
     setErrorMsg(event.target.validationMessage); // Actualiza el estado 'errorMsg' con el mensaje de error proporcionado por el navegador
     if (event.target.value.length < props.min) {
       setErrorMsg(`El valor ingresado es demasiado corto (mínimo ${props.min} caracteres)`); // Establece un mensaje de error personalizado si el valor es demasiado corto
-    } else if (event.target.value.length > props.max) {
-      setErrorMsg(`El valor ingresado es demasiado largo (máximo ${props.max} caracteres)`); // Establece un mensaje de error personalizado si el valor es demasiado largo
     } else if (event.target.value.match(props.regex)) {
       setErrorMsg(`El valor ingresado contiene caracteres no permitidos`); // Establece un mensaje de error personalizado si el valor contiene caracteres no permitidos
     } else {
@@ -48,10 +46,32 @@ export const CajaTexto = (props) => {
 };
 
 export const TextArea = (props) => {
+  const [isValid, setIsValid] = useState(true);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const handleInput = (event) => {
+    setIsValid(event.target.validity.valid); // Actualiza el estado 'isValid'
+    setErrorMsg(event.target.validationMessage); // Actualiza el estado 'errorMsg' con el mensaje de error proporcionado por el navegador
+    if (event.target.value.length < props.min) {
+      setErrorMsg(`El valor ingresado es demasiado corto (mínimo ${props.min} caracteres)`); // Establece un mensaje de error personalizado si el valor es demasiado corto
+    } else if (event.target.value.match(props.regex)) {
+      setErrorMsg(`El valor ingresado contiene caracteres no permitidos`); // Establece un mensaje de error personalizado si el valor contiene caracteres no permitidos
+    } else {
+      setErrorMsg(''); // Si no hay errores de validación, establece 'errorMsg' en una cadena vacía
+    }
+  };
+
+  const handleInvalid = (event) => {
+    setIsValid(false); // Establece el estado 'isValid' en 'false'
+    setErrorMsg(props.errorMsg || event.target.validationMessage); // Actualiza el estado 'errorMsg' con la propiedad 'errorMsg' proporcionada o el mensaje de error proporcionado por el navegador
+  };
   return (
     <div className="textarea">
       <label className="letras">{props.nombre}</label>
-      <textarea id={props.id} type="text" pattern={props.regex} maxLength={props.max} minLength={props.min} rows="3" cols="50" required/>
+      <textarea id={props.id} type="text" pattern={props.regex} maxLength={props.max} minLength={props.min} rows="3" cols="50" required  onInput={handleInput}
+        onInvalid={handleInvalid}/> {!isValid && (
+          <div className="errorMensaje">{errorMsg}</div>
+        )}
     </div>
   );
 }

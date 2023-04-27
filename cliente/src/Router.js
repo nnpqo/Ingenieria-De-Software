113d9a78f34370d6a57551bdcc310433261b0b06
Ivan Palacios <ivpalacios47@gmail.com>
2023-04-27 01:12:51 -0400

    busqueda 3.0!

--- cliente/src/Router.js
+++ cliente/src/Router.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 export const Router = () => {
   const [buscar, setBuscar] = useState(false)
+  
   const actualizarBusqueda = (nuevaBusqueda) => {
     setBuscar(nuevaBusqueda);
   }

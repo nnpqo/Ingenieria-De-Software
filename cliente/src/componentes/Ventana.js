ca3caa585f346d1c3e1ce0aefbbd0abd6b6aa5bd
Pie-De-Limon69 <choque2015li@gmail.com>
2023-05-01 09:05:28 -0400

    bug de mensaje de busqueda y mensaje de input

--- cliente/src/componentes/Ventana.js
+++ cliente/src/componentes/Ventana.js
@@ -228,7 +228,23 @@ export const Ventana = (props) => {
   return (
     <>
       <div className="ventana-productos">
-        {etiquetas.length === 0 ? <Bienvenida /> : mostrarProd}
+        {etiquetas.length === 0 ? (
+          <Bienvenida />
+        ) : (
+          <>
+            {prodFiltrado.length > 0 && (
+              <>
+                <div className="mens-bus">
+                  <p>
+                    Se encontraron {mostrarProd.length} modelos que coinciden
+                    con su busqueda
+                  </p>
+                </div>
+              </>
+            )}
+            <div className="pro">{mostrarProd}</div>
+          </>
+        )}
       </div>
     </>
   );

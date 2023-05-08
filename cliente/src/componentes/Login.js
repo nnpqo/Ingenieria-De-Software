c0811ae9c15eeff21a11d6f3214a777ab9c13a48
jesus~ <jesusbrere@gmail.com>
2023-05-08 19:39:29 -0400

    animacion Yanina-Jesus

--- cliente/src/componentes/Login.js
+++ cliente/src/componentes/Login.js
@@ -1,8 +1,36 @@
 import { Link } from "react-router-dom";
 import { Boton } from "./Boton"
+import { useState,useEffect } from "react";
+import "../estilos/login.css";
 
 export const Login =()=>{
-    return <div id="login"> 
+    let intervalId = null;
+
+    function createSquare(){
+        const section = document.querySelector('section');
+        const square = document.createElement('span');
+        square.classList.add('burbuja');
+        var size = Math.random() * 20;
+
+        square.style.width = 20 + size + 'px';
+        square.style.height = 20 + size + 'px';
+
+        square.style.top = Math.random() * window.innerHeight + 'px';
+        square.style.left = Math.random() * window.innerWidth + 'px';
+
+        section.appendChild(square);
+        setTimeout(() =>{
+            square.remove()
+        }, 5000)
+    }
+
+    useEffect(() => {
+        intervalId = setInterval(createSquare , 150)
+        return () => clearInterval(intervalId);
+    }, []);
+  
+    return <section id="login"> 
+            
         <div id="caja-login">
             <div id="titulo">
                 <h1 id="jdk">JDK</h1>
@@ -16,6 +44,25 @@ export const Login =()=>{
             <Link to="/home"> 
                 <Boton stilos={"guardar"} nombre={"acceder"}></Boton>
             </Link>
+                       
         </div>
-    </div>  
+        </section>
 }
+
+/*function createSquare(){
+    const section = document.querySelector('section');
+    const square = document.createElement('span');
+    var size = Math.random() * 20;
+    
+    square.style.width = 20 + size + 'px';
+    square.style.height = 20 + size + 'px';
+    
+      square.style.top = Math.random() * window.innerHeight + 'px';
+    square.style.left = Math.random() * window.innerWidth + 'px';
+    
+    section.appendChild(square);
+      setTimeout(() =>{
+      square.remove()
+    }, 5000)
+  }
+  setInterval(createSquare , 150)*/

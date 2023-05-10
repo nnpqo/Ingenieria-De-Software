d107ea22a59d84cba2cb928cfc0fd5d73851a65e
nnpqo <yaninapaolaquispeorellana@gmail.com>
2023-05-10 02:44:17 -0400

    implementacion de la ventana de venta css

--- cliente/src/componentes/VentanaMostrarVenta.js
+++ cliente/src/componentes/VentanaMostrarVenta.js
@@ -1,27 +1,43 @@
 import { Boton } from "./Boton"
 import samsung from "../imagenes/samsung_g.jpg";
+import "../estilos/VentanaMostrarVenta.css"
+import x from "../imagenes/cruz-peque.svg"
 export const VentanaMostrarVenta = () => {
     const lista = [{ruta:"",modelo:"IPHONE 13 PRO MAX", precio:"90000", marca:"APPLE", imei: 123456789012345, color: "blanco" },
-    {modelo:"IPHONE 13 PRO MAX", precio:"90000", marca:"APPLE", imei: 123456789012345, color: "blanco" },
     {modelo:"IPHONE 13 PRO MAX", precio:"90000", marca:"APPLE", imei: 123456789012345, color: "blanco" },
     {modelo:"IPHONE 13 PRO MAX", precio:"90000", marca:"APPLE", imei: 123456789012345, color: "blanco" }]
     let total=0;
     let list = lista.map((item) => {
         total += Number(item.precio);
-        return (
-            
+        return (  
         <div id={item.imei} className="fila">
-            <img  src={samsung} />
-            <p>MODELO:</p>
-            <p >{item.modelo}</p>
-            <p>MARCA:</p>
-            <p >{item.marca}</p>
-            <p>IMEI:</p>
-            <p >{item.imei}</p>
-            <p>COLOR:</p>
-            <p >{item.color}</p>
-            <p>PRECIO:</p>
-            <p >{item.precio}</p>
+            <div className="contenedor-img">
+            <img className="imagen-venta"  src={samsung} />
+            </div>
+            <div className="contenedor-mm">
+            <div className="modelo-venta">
+            <p id="dato">MODELO:</p>
+            <p id="argumento">{item.modelo}</p>
+            </div>
+            <div className="marca-venta">
+            <p id="dato">MARCA:</p>
+            <p id="argumento">{item.marca}</p>
+            </div>
+            </div>
+            <div className="contenedor-ic">
+            <div className="contenedor-imei">
+            <p id="dato">IMEI:</p>
+            <p id="argumento">{item.imei}</p>
+            </div>
+            <div className="contenedor-color">
+            <p id="dato">COLOR:</p>
+            <p id="argumento">{item.color}</p>
+            </div>
+            </div>
+            <div className="contenedor-precio">
+            <p id="dato">PRECIO:</p>
+            <p id="ar-precio">{item.precio}</p>
+            </div>
         </div>
         )
     })
@@ -31,9 +47,12 @@ export const VentanaMostrarVenta = () => {
             <div className="titulo-Ventana">
                 <h1>VENTA DE PRODUCTO</h1>
             </div>
+            <div className="centrar">
+            <div className="seguir-venta">
             <Boton nombre="SEGUIR VENDIENDO"
             estilos="guardar"
             />
+            </div>
             <div className="listaVenta">
                 {list}
             </div>
@@ -44,10 +63,12 @@ export const VentanaMostrarVenta = () => {
                 <Boton nombre="LIMPIAR"
                 estilos="cancelar"
                 />
-                <p>TOTAL:</p>
-                <p >{total}</p>
+                <div className="contenedor-total">
+                <p id="dato">TOTAL:</p>
+                <p id="ar-precio">{total}</p>
+                </div>
             </div>
-
+            </div>
 
 
         </div>

4bee6a3dde992c7da818829382e2b7bd5135b04c
Pie-De-Limon69 <choque2015li@gmail.com>
2023-05-24 22:16:25 -0400

    mensaje validacion user, password

--- cliente/src/componentes/Login.js
+++ cliente/src/componentes/Login.js
@@ -104,26 +104,7 @@ export const Login = (props) => {
           />
           <label className="labelLogin">Nombre de Usuario</label>
         </div>
-        <div className="errorMensaje">
-          <div className="fondo">
-            <div className="icon">
-              <svg
-                viewBox="0 0 24 24"
-                fill="#ffffff"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm-1.5-5.009c0-.867.659-1.491 1.491-1.491.85 0 1.509.624 1.509 1.491 0 .867-.659 1.509-1.509 1.509-.832 0-1.491-.642-1.491-1.509zM11.172 6a.5.5 0 0 0-.499.522l.306 7a.5.5 0 0 0 .5.478h1.043a.5.5 0 0 0 .5-.478l.305-7a.5.5 0 0 0-.5-.522h-1.655z"
-                  fill="#ff2e00"
-                />
-              </svg>
-            </div>
-          </div>
-          &nbsp; &nbsp; &nbsp; &nbsp;{errorMens}
-        </div>
-        {/* <p>{errorMens}</p> */}
+        <p className="mens-error">{errorMens}</p>
         <div className="input-grup">
           <div class="icon">
             <img src={candado} />
@@ -208,26 +189,7 @@ export const Login = (props) => {
           />
           <label className="labelLogin">Contraseña</label>
         </div>
-        <div className="errorMensaje">
-          <div className="fondo">
-            <div className="icon">
-              <svg
-                viewBox="0 0 24 24"
-                fill="#ffffff"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm-1.5-5.009c0-.867.659-1.491 1.491-1.491.85 0 1.509.624 1.509 1.491 0 .867-.659 1.509-1.509 1.509-.832 0-1.491-.642-1.491-1.509zM11.172 6a.5.5 0 0 0-.499.522l.306 7a.5.5 0 0 0 .5.478h1.043a.5.5 0 0 0 .5-.478l.305-7a.5.5 0 0 0-.5-.522h-1.655z"
-                  fill="#ff2e00"
-                />
-              </svg>
-            </div>
-          </div>
-          &nbsp; &nbsp; &nbsp; &nbsp;{errorMens}
-        </div>
-        {/* <p>{errorMens}</p> */}
+        <p className="mens-error">{errorMens}</p>
         <div className="input-grup">
           <Link className="acceder" to="/" onClick={validar}>
             <span></span>
@@ -265,5 +227,12 @@ const validate = (user, password) => {
   }
   if (password.length < 8 || password.length > 16) {
     return "La contraseña tiene que ser de 8 a 16 caracteres";
+  }
+  if (user !== "kevin") {
+    return "El usuario es incorrecto";
+  }
+
+  if (password !== "jdkcell") {
+    return "La contraseña es incorrecta";
   }
 };
